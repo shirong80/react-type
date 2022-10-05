a9571f218977e63d8dbe45770de7518d015f4c10
shirong80 <seyoung.kim@weraser.com>
2022-10-05 18:22:33 +0900

    first commit

---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from "styled-components";
+
+export const lightTheme: DefaultTheme = {
+  textColor: "black",
+  backgroundColor: "white",
+  btnColor: "tomato",
+};
+
+export const darkTheme: DefaultTheme = {
+  textColor: "white",
+  backgroundColor: "black",
+  btnColor: "teal",
+};
